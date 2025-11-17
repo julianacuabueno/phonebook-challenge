254c6c0a42ed198e19da9855b66add0877c545d9
julianacuabueno <julianacuabueno@gmail.com>
2025-11-17 17:19:41 -0500

    added more to form title

--- src/App.jsx
+++ src/App.jsx
@@ -190,7 +190,7 @@ const App = () => {
             </section>
 
             <section className="form" aria-labelledby="form-heading">
-                <h2 id="form-heading">add a contact</h2>
+                <h2 id="form-heading">. ₊˚ ☎︎₊˚✧  add a contact</h2>
                 <form className="form__body" onSubmit={handleSubmit} noValidate>
                     <div className="field">
                         <label htmlFor="name">name</label>
